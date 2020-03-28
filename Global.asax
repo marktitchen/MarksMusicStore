332b5abc6d4083cd78a9757310156a55f5d68996
Mark Titchen <46524303+marktitchen@users.noreply.github.com>
2020-03-28 20:51:42 +0000

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarksMusicStore.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarksMusicStore.Startup))]
+namespace MarksMusicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
