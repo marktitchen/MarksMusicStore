2b08567ddeacbb15b197177687ec5e45163a8b81
Mark Titchen <46524303+marktitchen@users.noreply.github.com>
2020-03-29 14:07:56 +0100

    5.Generic unit of work, IReposity and GeneriReposity created.

---
+++ Repository/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MarksMusicStore.Repository
+{
+	public interface IRepository<Tbl_Entity> where Tbl_Entity:class
+	{
+		IEnumerable<Tbl_Entity> GetAllRecords();
+		IQueryable<Tbl_Entity> GetAllRecordsIQueryable();
+		int GetAllRecordsCount();
+		void All(Tbl_Entity entity);
+		void Update(Tbl_Entity entity);
+		void UpdateByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict);
+		Tbl_Entity GetFirstorDefault(int recordId);
+		void Remove(Tbl_Entity entity);
+		void RemoveByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict);
+		void RemoveRangeByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict);
+		void InactiveAndDeleteMarkByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict);
+		Tbl_Entity GetFirstorDefaultByParameter(Expression<Func<Tbl_Entity, bool>> wherePredict);
+		IEnumerable<Tbl_Entity> GetListParameter(Expression<Func<Tbl_Entity, bool>> wherePredict);
+		IEnumerable<Tbl_Entity> GetResultBySqlprocedure(string query, params object[] parameters);
+
+		IEnumerable<Tbl_Entity> GetRecordsToShow(int PageNo, int PageSize, int CurrentPage, Expression<Func<Tbl_Entity, bool>> wherePredict, Expression<Func<Tbl_Entity, int>> orderByPredict);
+
+
+	}
+}
