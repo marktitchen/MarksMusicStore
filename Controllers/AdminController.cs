adde2cab249e0eaab686562002af39b1e91f94fe
Mark Titchen <46524303+marktitchen@users.noreply.github.com>
2020-03-29 16:31:25 +0100

    6. Admin dashboard created

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using MarksMusicStore.DAL;
+using MarksMusicStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarksMusicStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+    }
+}
+
